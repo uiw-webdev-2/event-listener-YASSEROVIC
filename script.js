a0766b759ae79cbc05349e39e813eeab118edc5a
YASSEROVIC <yasserovic@gmail.com>
2022-03-31 19:39:50 -0500

    type name, edit again - event listener
    using JS,CSS :
    1. type your name in the input form.
    2. you will get alert writing ( "you type the Name : " + text you typed.
    3.  at same time will hide all and change background color.
    4. new button will be visible ( edit your name )
    5. you will get back to first step to change name you entered.
    at the same time edit button will be hidden after clicked
    CSS:
    center page, color background and margin top also edit button

--- script.js
+++ script.js
@@ -3,9 +3,24 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/Events
  */
 
-const container = document.querySelector(".container");
-const button = document.querySelector(".cta-button");
+ const message = document.getElementById("formName");
+ const btn = document.getElementById('myButton');
 
-// Log when the button is clicked in the console.
-button.addEventListener("click", () => {
-}, false);
+// first click to alert with name
+document.getElementById("myButton").addEventListener("click", function() {
+    document.body.style.backgroundColor = 'salmon';
+    document.getElementById("formName").style.visibility = "hidden";
+    document.getElementById("myButton").style.visibility = "hidden";
+    document.querySelector("h1").style.visibility = "hidden";
+    alert("you type the Name : " + message.value);
+    document.getElementById("buttonEdit").style.visibility = "visible";
+    return false;
+});
+document.getElementById("buttonEdit").addEventListener("click", function() {
+    document.body.style.backgroundColor = 'bisque';
+    document.getElementById("formName").style.visibility = "visible";
+    document.getElementById("myButton").style.visibility = "visible";
+    document.querySelector("h1").style.visibility = "visible";
+    document.getElementById("buttonEdit").style.visibility = "hidden";
+    return false;
+});
